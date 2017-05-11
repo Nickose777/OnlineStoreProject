a026127fd23be07dc9bc7bfcc0ea019a1a1d193d
Nickose777 <my_personal@mail.ua>
2017-05-11 22:42:38 +0300

    Checkout post validation done

--- catalog/controller/custom/checkout.php
+++ catalog/controller/custom/checkout.php
@@ -5,11 +5,346 @@ class ControllerCustomCheckout extends Controller {
 		$this->document->setDescription($this->config->get('config_meta_description'));
 		$this->document->setKeywords($this->config->get('config_meta_keyword'));
 
+		$data['entry_firstname'] = "First name";
+		$data['entry_lastname'] = "Last name";
+		$data['entry_email'] = "E-Mail";
+		$data['entry_telephone'] = "Phone number";
+		$data['entry_company'] = "Company";
+		$data['entry_address_1'] = "Address";
+		$data['entry_city'] = "City";
+		$data['entry_postcode'] = "Postcode";
+		$data['entry_country'] = "Country";
+		$data['entry_zone'] = "Region";
+
+		$data['text_payment_method'] = "Payment method";
+
+		$data['text_select'] = "Select";
+		$data['text_none'] = "None";
 
 		$data['footer'] = $this->load->controller('common/footer');
 		$data['header'] = $this->load->controller('common/header');
+
+		//Session
+		if (isset($this->session->data['guest']['customer_group_id'])) {
+			$data['customer_group_id'] = $this->session->data['guest']['customer_group_id'];
+		} else {
+			$data['customer_group_id'] = $this->config->get('config_customer_group_id');
+		}
+
+		if (isset($this->session->data['guest']['firstname'])) {
+			$data['firstname'] = $this->session->data['guest']['firstname'];
+		} else {
+			$data['firstname'] = '';
+		}
+
+		if (isset($this->session->data['guest']['lastname'])) {
+			$data['lastname'] = $this->session->data['guest']['lastname'];
+		} else {
+			$data['lastname'] = '';
+		}
+
+		if (isset($this->session->data['guest']['email'])) {
+			$data['email'] = $this->session->data['guest']['email'];
+		} else {
+			$data['email'] = '';
+		}
+
+		if (isset($this->session->data['guest']['telephone'])) {
+			$data['telephone'] = $this->session->data['guest']['telephone'];
+		} else {
+			$data['telephone'] = '';
+		}
+
+		if (isset($this->session->data['guest']['fax'])) {
+			$data['fax'] = $this->session->data['guest']['fax'];
+		} else {
+			$data['fax'] = '';
+		}
+
+		if (isset($this->session->data['payment_address']['company'])) {
+			$data['company'] = $this->session->data['payment_address']['company'];
+		} else {
+			$data['company'] = '';
+		}
+
+		if (isset($this->session->data['payment_address']['address_1'])) {
+			$data['address_1'] = $this->session->data['payment_address']['address_1'];
+		} else {
+			$data['address_1'] = '';
+		}
+
+		if (isset($this->session->data['payment_address']['postcode'])) {
+			$data['postcode'] = $this->session->data['payment_address']['postcode'];
+		} elseif (isset($this->session->data['shipping_address']['postcode'])) {
+			$data['postcode'] = $this->session->data['shipping_address']['postcode'];
+		} else {
+			$data['postcode'] = '';
+		}
+
+		if (isset($this->session->data['payment_address']['city'])) {
+			$data['city'] = $this->session->data['payment_address']['city'];
+		} else {
+			$data['city'] = '';
+		}
+
+		if (isset($this->session->data['payment_address']['country_id'])) {
+			$data['country_id'] = $this->session->data['payment_address']['country_id'];
+		} elseif (isset($this->session->data['shipping_address']['country_id'])) {
+			$data['country_id'] = $this->session->data['shipping_address']['country_id'];
+		} else {
+			$data['country_id'] = $this->config->get('config_country_id');
+		}
+
+		if (isset($this->session->data['payment_address']['zone_id'])) {
+			$data['zone_id'] = $this->session->data['payment_address']['zone_id'];
+		} elseif (isset($this->session->data['shipping_address']['zone_id'])) {
+			$data['zone_id'] = $this->session->data['shipping_address']['zone_id'];
+		} else {
+			$data['zone_id'] = '';
+		}
+
+		//Countries & Regions
+		$country_id = 81;//Germany
+
+		// Totals
+		$totals = array();
+		$taxes = $this->cart->getTaxes();
+		$total = 0;
+
+		// Because __call can not keep var references so we put them into an array.
+		$total_data = array(
+			'totals' => &$totals,
+			'taxes'  => &$taxes,
+			'total'  => &$total
+		);
+		
+		$this->load->model('localisation/country');
+		$this->load->model('extension/extension');
+
+		$sort_order = array();
+
+		$results = $this->model_extension_extension->getExtensions('total');
+
+		foreach ($results as $key => $value) {
+			$sort_order[$key] = $this->config->get($value['code'] . '_sort_order');
+		}
+
+		array_multisort($sort_order, SORT_ASC, $results);
+
+		foreach ($results as $result) {
+			if ($this->config->get($result['code'] . '_status')) {
+				$this->load->model('extension/total/' . $result['code']);
+				
+				// We have to put the totals in an array so that they pass by reference.
+				$this->{'model_extension_total_' . $result['code']}->getTotal($total_data);
+			}
+		}
+
+		// Payment Methods
+		$method_data = array();
+
+
+		$results = $this->model_extension_extension->getExtensions('payment');
+
+		$recurring = $this->cart->hasRecurringProducts();
+
+		foreach ($results as $result) {
+			if ($this->config->get($result['code'] . '_status')) {
+				$this->load->model('extension/payment/' . $result['code']);
+
+				$address = array(
+					'country_id' => $country_id,
+					'zone_id' => 0
+					);
+				$method = $this->{'model_extension_payment_' . $result['code']}->getMethod($address, $total);
+
+				if ($method) {
+					if ($recurring) {
+						if (property_exists($this->{'model_extension_payment_' . $result['code']}, 'recurringPayments') && $this->{'model_extension_payment_' . $result['code']}->recurringPayments()) {
+							$method_data[$result['code']] = $method;
+						}
+					} else {
+						$method_data[$result['code']] = $method;
+					}
+				}
+			}
+		}
+
+		$sort_order = array();
+
+		foreach ($method_data as $key => $value) {
+			$sort_order[$key] = $value['sort_order'];
+		}
+
+		array_multisort($sort_order, SORT_ASC, $method_data);
+
+		$this->session->data['payment_methods'] = $method_data;
+
+		$data['countries'] = $this->model_localisation_country->getCountries();
+		$data['country_id'] = $country_id;
+		$data['payment_methods'] = $method_data;
 		
 		$this->response->setOutput($this->load->view('custom/checkout', $data));
+	}
+
+	public function confirm() {
+		$json = array();
+		$validationSucceeded = true;
+
+		$this->load->language('checkout/checkout');
+
+		if ($this->cart->hasProducts()) {
+			if ((utf8_strlen(trim($this->request->post['firstname'])) < 1) || (utf8_strlen(trim($this->request->post['firstname'])) > 32)) {
+				$json['error']['firstname'] = $this->language->get('error_firstname');
+				$validationSucceeded = false;
+			}
+
+			if ((utf8_strlen(trim($this->request->post['lastname'])) < 1) || (utf8_strlen(trim($this->request->post['lastname'])) > 32)) {
+				$json['error']['lastname'] = $this->language->get('error_lastname');
+				$validationSucceeded = false;
+			}
+
+			if ((utf8_strlen($this->request->post['email']) > 96) || !filter_var($this->request->post['email'], FILTER_VALIDATE_EMAIL)) {
+				$json['error']['email'] = $this->language->get('error_email');
+				$validationSucceeded = false;
+			}
+
+			if ((utf8_strlen($this->request->post['telephone']) < 3) || (utf8_strlen($this->request->post['telephone']) > 32)) {
+				$json['error']['telephone'] = $this->language->get('error_telephone');
+				$validationSucceeded = false;
+			}
+
+			if ((utf8_strlen(trim($this->request->post['address_1'])) < 3) || (utf8_strlen(trim($this->request->post['address_1'])) > 128)) {
+				$json['error']['address_1'] = $this->language->get('error_address_1');
+				$validationSucceeded = false;
+			}
+
+			if ((utf8_strlen(trim($this->request->post['city'])) < 2) || (utf8_strlen(trim($this->request->post['city'])) > 128)) {
+				$json['error']['city'] = $this->language->get('error_city');
+				$validationSucceeded = false;
+			}
+
+			$this->load->model('localisation/country');
+
+			$country_info = $this->model_localisation_country->getCountry($this->request->post['country_id']);
+
+			if ($country_info && $country_info['postcode_required'] && (utf8_strlen(trim($this->request->post['postcode'])) < 2 || utf8_strlen(trim($this->request->post['postcode'])) > 10)) {
+				$json['error']['postcode'] = $this->language->get('error_postcode');
+				$validationSucceeded = false;
+			}
+
+			if ($this->request->post['country_id'] == '') {
+				$json['error']['country'] = $this->language->get('error_country');
+				$validationSucceeded = false;
+			}
+
+			if (!isset($this->request->post['zone_id']) || $this->request->post['zone_id'] == '' || !is_numeric($this->request->post['zone_id'])) {
+				$json['error']['zone'] = $this->language->get('error_zone');
+				$validationSucceeded = false;
+			}
+		}
+		else {
+			$json['redirect'] = $this->url->link('custom/cart', '', true);
+			$validationSucceeded = false;
+		}
+
+		if ($validationSucceeded) {
+			$this->session->data['account'] = 'guest';
+			$this->session->data['guest']['customer_group_id'] = $this->config->get('config_customer_group_id');;
+			$this->session->data['guest']['firstname'] = $this->request->post['firstname'];
+			$this->session->data['guest']['lastname'] = $this->request->post['lastname'];
+			$this->session->data['guest']['email'] = $this->request->post['email'];
+			$this->session->data['guest']['telephone'] = $this->request->post['telephone'];
+			$this->session->data['guest']['fax'] = "";
+			$this->session->data['guest']['custom_field'] = array();
+
+			$this->session->data['payment_address']['firstname'] = $this->request->post['firstname'];
+			$this->session->data['payment_address']['lastname'] = $this->request->post['lastname'];
+			$this->session->data['payment_address']['company'] = $this->request->post['company'];
+			$this->session->data['payment_address']['address_1'] = $this->request->post['address_1'];
+			$this->session->data['payment_address']['address_2'] = "";
+			$this->session->data['payment_address']['postcode'] = $this->request->post['postcode'];
+			$this->session->data['payment_address']['city'] = $this->request->post['city'];
+			$this->session->data['payment_address']['country_id'] = $this->request->post['country_id'];
+			$this->session->data['payment_address']['zone_id'] = $this->request->post['zone_id'];
+
+			$this->load->model('localisation/country');
+
+			$country_info = $this->model_localisation_country->getCountry($this->request->post['country_id']);
+
+			$country = $country_info ? $country_info['name'] : '';
+			$iso_code_2 = $country_info ? $country_info['iso_code_2'] : '';
+			$iso_code_3 = $country_info ? $country_info['iso_code_3'] : '';
+			$address_format = $country_info ? $country_info['address_format'] : '';
+
+			$this->session->data['payment_address']['country'] = $country;
+			$this->session->data['payment_address']['iso_code_2'] = $iso_code_2;
+			$this->session->data['payment_address']['iso_code_3'] = $iso_code_3;
+			$this->session->data['payment_address']['address_format'] = $address_format;
+			$this->session->data['payment_address']['custom_field'] = array();
+
+			$this->load->model('localisation/zone');
+
+			$zone_info = $this->model_localisation_zone->getZone($this->request->post['zone_id']);
+
+			$zone_name = $zone_info ? $zone_info['name'] : '';
+			$zone_code = $zone_info ? $zone_info['code'] : '';
+
+			$this->session->data['payment_address']['zone'] = $zone_name;
+			$this->session->data['payment_address']['zone_code'] = $zone_code;
+
+			if (!empty($this->request->post['shipping_address'])) {
+				$this->session->data['guest']['shipping_address'] = $this->request->post['shipping_address'];
+			} else {
+				$this->session->data['guest']['shipping_address'] = false;
+			}
+
+			if ($this->session->data['guest']['shipping_address']) {
+				$this->session->data['shipping_address']['firstname'] = $this->request->post['firstname'];
+				$this->session->data['shipping_address']['lastname'] = $this->request->post['lastname'];
+				$this->session->data['shipping_address']['company'] = $this->request->post['company'];
+				$this->session->data['shipping_address']['address_1'] = $this->request->post['address_1'];
+				$this->session->data['shipping_address']['address_2'] = "";
+				$this->session->data['shipping_address']['postcode'] = $this->request->post['postcode'];
+				$this->session->data['shipping_address']['city'] = $this->request->post['city'];
+				$this->session->data['shipping_address']['country_id'] = $this->request->post['country_id'];
+				$this->session->data['shipping_address']['zone_id'] = $this->request->post['zone_id'];
+
+				if ($country_info) {
+					$this->session->data['shipping_address']['country'] = $country_info['name'];
+					$this->session->data['shipping_address']['iso_code_2'] = $country_info['iso_code_2'];
+					$this->session->data['shipping_address']['iso_code_3'] = $country_info['iso_code_3'];
+					$this->session->data['shipping_address']['address_format'] = $country_info['address_format'];
+				} else {
+					$this->session->data['shipping_address']['country'] = '';
+					$this->session->data['shipping_address']['iso_code_2'] = '';
+					$this->session->data['shipping_address']['iso_code_3'] = '';
+					$this->session->data['shipping_address']['address_format'] = '';
+				}
+
+				if ($zone_info) {
+					$this->session->data['shipping_address']['zone'] = $zone_info['name'];
+					$this->session->data['shipping_address']['zone_code'] = $zone_info['code'];
+				} else {
+					$this->session->data['shipping_address']['zone'] = '';
+					$this->session->data['shipping_address']['zone_code'] = '';
+				}
+
+				if (isset($this->request->post['custom_field']['address'])) {
+					$this->session->data['shipping_address']['custom_field'] = $this->request->post['custom_field']['address'];
+				} else {
+					$this->session->data['shipping_address']['custom_field'] = array();
+				}
+			}
+
+			unset($this->session->data['shipping_method']);
+			unset($this->session->data['shipping_methods']);
+			unset($this->session->data['payment_method']);
+			unset($this->session->data['payment_methods']);
+		}
+
+		$this->response->addHeader('Content-Type: application/json');
+		$this->response->setOutput(json_encode($json));
 	}
 }
 ?>
--- catalog/view/theme/default/template/custom/checkout.tpl
+++ catalog/view/theme/default/template/custom/checkout.tpl
@@ -1,67 +1,151 @@
 <?php echo $header; ?>
 	<div id="content">
 		<form>
-			<div class="form-group row">
-      <label for="inputEmail3" class="col-sm-2 col-form-label" style="padding-left: 60px; font-weight: bold; font-size: 15px;">NAME</label>
-      <div class="col-sm-3">
-        <input type="email" class="form-control" id="inputEmail3" placeholder="Name">
+      <div class="form-group required">
+        <label class="control-label" for="input-payment-firstname"><?php echo $entry_firstname; ?></label>
+        <input type="text" name="firstname" placeholder="<?php echo $entry_firstname; ?>" value="<?php echo $firstname; ?>" id="input-payment-firstname" class="form-control" />
       </div>
-    </div>
-    <div class="form-group row">
-      <label for="inputPassword3" class="col-sm-2 col-form-label" style="padding-left: 60px; font-weight: bold; font-size: 15px;">SURNAME</label>
-      <div class="col-sm-3">
-        <input type="password" class="form-control" id="inputPassword3" placeholder="Surname">
+      <div class="form-group required">
+        <label class="control-label" for="input-payment-lastname"><?php echo $entry_lastname; ?></label>
+        <input type="text" name="lastname" placeholder="<?php echo $entry_lastname; ?>" value="<?php echo $lastname; ?>" id="input-payment-lastname" class="form-control" />
       </div>
-    </div>
-    <div class="form-group row">
-      <label for="inputEmail3" class="col-sm-2 col-form-label" style="padding-left: 60px; font-weight: bold; font-size: 15px;">EMAIL</label>
-      <div class="col-sm-3">
-        <input type="email" class="form-control" id="inputEmail3" placeholder="Email">
+      <div class="form-group required">
+        <label class="control-label" for="input-payment-email"><?php echo $entry_email; ?></label>
+        <input type="text" name="email" placeholder="<?php echo $entry_email; ?>" value="<?php echo $email; ?>" id="input-payment-email" class="form-control" />
       </div>
-    </div>
-    <div class="form-group row">
-      <label for="inputPassword3" class="col-sm-2 col-form-label" style="padding-left: 60px; font-weight: bold; font-size: 15px;">PHONENUMBER</label>
-      <div class="col-sm-3">
-        <input type="password" class="form-control" id="inputPassword3" placeholder="Phonenumber">
+      <div class="form-group required">
+        <label class="control-label" for="input-payment-telephone"><?php echo $entry_telephone; ?></label>
+        <input type="text" name="telephone" placeholder="<?php echo $entry_telephone; ?>" value="<?php echo $telephone; ?>" id="input-payment-telephone" class="form-control" />
       </div>
-    </div>
-    <div class="form-group row">
-      <label for="inputEmail3" class="col-sm-2 col-form-label" style="padding-left: 60px; font-weight: bold; font-size: 15px;">ADRESS</label>
-      <div class="col-sm-3">
-        <input type="email" class="form-control" id="inputEmail3" placeholder="Adress">
+      <div class="form-group">
+        <label class="control-label" for="input-payment-company"><?php echo $entry_company; ?></label>
+        <input type="text" name="company" placeholder="<?php echo $entry_company; ?>" value="<?php echo $company; ?>" id="input-payment-company" class="form-control" />
       </div>
-    </div>
-    <div class="form-group row">
-      <label for="inputPassword3" class="col-sm-2 col-form-label" style="padding-left: 60px; font-weight: bold; font-size: 15px;">CITY</label>
-      <div class="col-sm-3">
-        <input type="password" class="form-control" id="inputPassword3" placeholder="City">
+      <div class="form-group required">
+        <label class="control-label" for="input-payment-address-1"><?php echo $entry_address_1; ?></label>
+        <input type="text" name="address_1" placeholder="<?php echo $entry_address_1; ?>" value="<?php echo $address_1; ?>" id="input-payment-address-1" class="form-control" />
       </div>
-    </div>
-    <div class="form-group row">
-      <label for="inputEmail3" class="col-sm-2 col-form-label" style="padding-left: 60px; font-weight: bold; font-size: 15px;">COUNTRY</label>
-      <div class="col-sm-3">
-        <input type="email" class="form-control" id="inputEmail3" placeholder="Country">
+      <div class="form-group required">
+        <label class="control-label" for="input-payment-city"><?php echo $entry_city; ?></label>
+        <input type="text" name="city" placeholder="<?php echo $entry_city; ?>" value="<?php echo $city; ?>" id="input-payment-city" class="form-control" />
       </div>
-    </div>
-    <div class="form-group row">
-      <label for="inputPassword3" class="col-sm-2 col-form-label" style="padding-left: 60px; font-weight: bold; font-size: 15px;">POST CODE</label>
-      <div class="col-sm-3">
-        <input type="password" class="form-control" id="inputPassword3" placeholder="Post code">
+      <div class="form-group required">
+        <label class="control-label" for="input-payment-postcode"><?php echo $entry_postcode; ?></label>
+        <input type="text" name="postcode" placeholder="<?php echo $entry_postcode; ?>" value="<?php echo $postcode; ?>" id="input-payment-postcode" class="form-control" />
       </div>
-    </div>			
+      <div class="form-group required">
+        <label class="control-label" for="input-payment-country"><?php echo $entry_country; ?></label>
+        <select name="country_id" id="input-payment-country" class="form-control">
+          <option value=""><?php echo $text_select; ?></option>
+          <?php foreach ($countries as $country) { ?>
+          <?php if ($country['country_id'] == $country_id) { ?>
+          <option value="<?php echo $country['country_id']; ?>" selected="selected"><?php echo $country['name']; ?></option>
+          <?php } else { ?>
+          <option value="<?php echo $country['country_id']; ?>"><?php echo $country['name']; ?></option>
+          <?php } ?>
+          <?php } ?>
+        </select>
+      </div>
+      <div class="form-group required">
+        <label class="control-label" for="input-payment-zone"><?php echo $entry_zone; ?></label>
+        <select name="zone_id" id="input-payment-zone" class="form-control">
+        </select>
+      </div>	
 			
-	<div class="form-check">
-		<p style="padding-left: 40px; font-weight: bold; font-size: 15px;">PAYMENT METHOD</p>
-		<p style="padding-left: 60px;"><input type="radio" name="Payment Method" value="&&&&&&&&&&">&&&&&&&&&</p>
-		<p style="padding-left: 60px;"><input type="radio" name="Payment Method" value="&&&&&&&&&&" checked="">&&&&&&&&&&</p>
-		<p style="padding-left: 40px; font-weight: bold; font-size: 15px;">DELIVERY METHOD</p>
-		<p style="padding-left: 60px;"><input type="radio" name="Delivery Method" value="&&&&&&&&&&">$$$$$$$</p>
-		<p style="padding-left: 60px;"><input type="radio" name="Delivery Method" value="&&&&&&&&&&" checked="">$$$$$$$$$$</p>
-	</div>
+    	<div class="form-check">
+    		<?php if ($payment_methods) { ?>
+        <p><?php echo $text_payment_method; ?></p>
+        <?php foreach ($payment_methods as $payment_method) { ?>
+        <div class="radio">
+          <label>
+            <input type="radio" name="payment_method" value="<?php echo $payment_method['code']; ?>" checked="checked" />
+            <?php echo $payment_method['title']; ?>
+            <?php if ($payment_method['terms']) { ?>
+            (<?php echo $payment_method['terms']; ?>)
+            <?php } ?>
+          </label>
+        </div>
+        <?php } ?>
+        <?php } ?>
+    	</div>
 
-			<button type="button" class="btn btn-default center-block btn btn-success btn-lg" style="font-size: 20px; width: 150px; border-radius: 15px">Order</button><br>
+			<button type="button" id="confirm_order" class="btn btn-default center-block btn btn-success btn-lg" style="font-size: 20px; width: 150px; border-radius: 15px">Order</button><br>
 			<button type="button" class="btn btn-default center-block" style="font-size: 20px; width: 150px; border-radius: 15px">Back</button>	
 		</form>
-	</div>
-					
+	</div>	
 <?php echo $footer; ?>
+
+<script type="text/javascript"><!--
+$('select[name=\'country_id\']').on('change', function() {
+  $.ajax({
+    url: 'index.php?route=checkout/checkout/country&country_id=' + this.value,
+    dataType: 'json',
+    beforeSend: function() {
+      $('#collapse-payment-address select[name=\'country_id\']').after(' <i class="fa fa-circle-o-notch fa-spin"></i>');
+    },
+    complete: function() {
+      $('.fa-spin').remove();
+    },
+    success: function(json) {
+      if (json['postcode_required'] == '1') {
+        $('input[name=\'postcode\']').parent().parent().addClass('required');
+      } else {
+        $('input[name=\'postcode\']').parent().parent().removeClass('required');
+      }
+
+      html = '<option value=""><?php echo $text_select; ?></option>';
+
+      if (json['zone'] && json['zone'] != '') {
+        for (i = 0; i < json['zone'].length; i++) {
+          html += '<option value="' + json['zone'][i]['zone_id'] + '"';
+
+          if (json['zone'][i]['zone_id'] == '<?php echo $zone_id; ?>') {
+            html += ' selected="selected"';
+          }
+
+          html += '>' + json['zone'][i]['name'] + '</option>';
+        }
+      } else {
+        html += '<option value="0" selected="selected"><?php echo $text_none; ?></option>';
+      }
+
+      $('select[name=\'zone_id\']').html(html);
+    },
+    error: function(xhr, ajaxOptions, thrownError) {
+      alert(thrownError + "\r\n" + xhr.statusText + "\r\n" + xhr.responseText);
+    }
+  });
+});
+
+$('select[name=\'country_id\']').trigger('change');
+//--></script>
+
+<script>
+  $(document).delegate('#confirm_order', 'click', function() {
+      $.ajax({
+          url: 'index.php?route=custom/checkout/confirm',
+          type: 'post',
+          data: $('input[type=\'text\'], input[type=\'checkbox\']:checked, input[type=\'radio\']:checked, select'),
+          dataType: 'json',
+          beforeSend: function() {
+            //$('#confirm_order').button('loading');
+        },
+          success: function(json) {
+            if (json['redirect']) {
+              location = json['redirect'];
+            } 
+            else if (json['error']) {
+              //TODO
+              //errors
+              alert("Some errors");
+            }
+            else {
+              alert("No errors");
+            }
+        },
+          error: function(xhr, ajaxOptions, thrownError) {
+              alert(thrownError + "\r\n" + xhr.statusText + "\r\n" + xhr.responseText);
+          }
+      });
+  });
+</script>
